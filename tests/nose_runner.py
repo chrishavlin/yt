5af7ec5a3f222a33462908b543d642ff89e1050e
chavlin <chris.havlin@gmail.com>
2024-06-18 14:04:00 -0500

    DO NOT MERGE.

--- tests/nose_runner.py
+++ tests/nose_runner.py
@@ -80,8 +80,8 @@ def generate_tasks_input():
 
     args = []
 
-    for test in list(tests["other_tests"].keys()):
-        args.append(([test] + base_argv + tests["other_tests"][test], True))
+    # for test in list(tests["other_tests"].keys()):
+    #     args.append(([test] + base_argv + tests["other_tests"][test], True))
     for answer in list(tests["answer_tests"].keys()):
         if tests["answer_tests"][answer] is None:
             continue
@@ -91,46 +91,52 @@ def generate_tasks_input():
         argv += tests["answer_tests"][answer]
         args.append((argv, False))
 
-    exclude_answers = []
-    answer_tests = tests["answer_tests"]
-    for key in answer_tests:
-        for t in answer_tests[key]:
-            exclude_answers.append(t.replace(".py:", ".").replace("/", "."))
-    exclude_answers = [f"--exclude-test={ex}" for ex in exclude_answers]
-
-    args = [
-        (
-            (item + [f"--xunit-file={item[0]}.xml"], exclusive)
-            if item[0] != "unittests"
-            else (item + ["--xunit-file=unittests.xml"] + exclude_answers, exclusive)
-        )
-        for item, exclusive in args
-    ]
+    # exclude_answers = []
+    # answer_tests = tests["answer_tests"]
+    # for key in answer_tests:
+    #     for t in answer_tests[key]:
+    #         exclude_answers.append(t.replace(".py:", ".").replace("/", "."))
+    # exclude_answers = [f"--exclude-test={ex}" for ex in exclude_answers]
+    #
+    # args = [
+    #     (
+    #         (item + [f"--xunit-file={item[0]}.xml"], exclusive)
+    #         if item[0] != "unittests"
+    #         else (item + ["--xunit-file=unittests.xml"] + exclude_answers, exclusive)
+    #     )
+    #     for item, exclusive in args
+    # ]
     return args
 
 
 if __name__ == "__main__":
-    # multiprocessing.log_to_stderr(logging.DEBUG)
-    tasks = multiprocessing.JoinableQueue()
-    results = multiprocessing.Queue()
 
-    num_consumers = int(os.environ.get("NUM_WORKERS", 6))
-    consumers = [NoseWorker(tasks, results) for i in range(num_consumers)]
-    for w in consumers:
-        w.start()
+    if len(sys.argv)==1:
+        # multiprocessing.log_to_stderr(logging.DEBUG)
+        tasks = multiprocessing.JoinableQueue()
+        results = multiprocessing.Queue()
 
-    num_jobs = 0
-    for job in generate_tasks_input():
-        if job[1]:
-            num_consumers -= 1  # take into account exclusive jobs
-        tasks.put(NoseTask(job))
-        num_jobs += 1
+        num_consumers = int(os.environ.get("NUM_WORKERS", 6))
+        consumers = [NoseWorker(tasks, results) for i in range(num_consumers)]
+        for w in consumers:
+            w.start()
 
-    for _i in range(num_consumers):
-        tasks.put(None)
+        num_jobs = 0
+        for job in generate_tasks_input():
+            if job[1]:
+                num_consumers -= 1  # take into account exclusive jobs
+            tasks.put(NoseTask(job))
+            num_jobs += 1
 
-    tasks.join()
+        for _i in range(num_consumers):
+            tasks.put(None)
 
-    while num_jobs:
-        result = results.get()
-        num_jobs -= 1
+        tasks.join()
+
+        while num_jobs:
+            result = results.get()
+            num_jobs -= 1
+
+    else:
+        for job in generate_tasks_input():
+            print(job)
