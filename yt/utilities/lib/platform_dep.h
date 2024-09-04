f3ee11460bc82b9bbb87d2bd1f09e90759b97c18
chavlin <chris.havlin@gmail.com>
2024-09-04 12:06:34 -0500

    hmm

--- yt/utilities/lib/platform_dep.h
+++ yt/utilities/lib/platform_dep.h
@@ -2,6 +2,7 @@
 #ifdef MS_WIN32
 #include "malloc.h"
 #include <float.h>
+#include <math.h>
 typedef int int32_t;
 typedef long long int64_t;
 /* Taken from http://siliconandlithium.blogspot.com/2014/05/msvc-c99-mathh-header.html */
@@ -30,30 +31,30 @@ static __inline double fmin(double x, double y){
 /* adapted from http://www.johndcook.com/blog/cpp_erf/
    code is under public domain license */
 
-double erf(double x)
-{
-    /* constants */
-    double a1 =  0.254829592;
-    double a2 = -0.284496736;
-    double a3 =  1.421413741;
-    double a4 = -1.453152027;
-    double a5 =  1.061405429;
-    double p  =  0.3275911;
-    double t;
-    double y;
-
-    /* Save the sign of x */
-    int sign = 1;
-    if (x < 0)
-        sign = -1;
-    x = fabs(x);
-
-    /* A&S formula 7.1.26 */
-    t = 1.0/(1.0 + p*x);
-    y = 1.0 - (((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t*exp(-x*x);
-
-    return sign*y;
-}
+//double erf(double x)
+//{
+//    /* constants */
+//    double a1 =  0.254829592;
+//    double a2 = -0.284496736;
+//    double a3 =  1.421413741;
+//    double a4 = -1.453152027;
+//    double a5 =  1.061405429;
+//    double p  =  0.3275911;
+//    double t;
+//    double y;
+//
+//    /* Save the sign of x */
+//    int sign = 1;
+//    if (x < 0)
+//        sign = -1;
+//    x = fabs(x);
+//
+//    /* A&S formula 7.1.26 */
+//    t = 1.0/(1.0 + p*x);
+//    y = 1.0 - (((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t*exp(-x*x);
+//
+//    return sign*y;
+//}
 #elif defined(__FreeBSD__)
 #include <stdint.h>
 #include <stdlib.h>
